06aef7076f02d6f455f4a5e2df2e5e24b992dec5
Takoda Register <Takodr2@gmail.com>
2019-01-03 21:44:13 -0500

    -If a user enters an invalid zip code we now display a message letting the user know

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,9 +2,8 @@ import React, {Component} from 'react';
 import {Platform, View, Text, StyleSheet, Image, ScrollView, TextInput, Button, TouchableHighlight} from 'react-native'
 const options = {weekday: 'long', month: 'long', day: 'numeric'};
 import { Constants, Location, Permissions } from 'expo';
-var zipCodeEntered = false;
 
-function myWeather(weather) {
+function RenderSevenDayWeather(weather, zipCodeTextInput) {
     if (typeof weather !== 'undefined') {
         return <View style={{flexDirection: 'row'}}><ScrollView horizontal={true} style={styles.container}>
             {Object.entries(weather).map(([key, weathers]) =>
@@ -22,17 +21,18 @@ function myWeather(weather) {
             )}
         </ScrollView></View>;
     }
-    // else {
-    //     return <View><Text> is not a valid zip code please enter another one</Text></View>
-    // }
+    else if(zipCodeTextInput.length > 0) {
+        return <View style={styles.invalidZipCodeContainer}>
+                    <Text style={styles.invalidZipCode}> {zipCodeTextInput}</Text>
+                    <Text style={styles.invalZipCodeMessage}>  is not a valid zip code :( </Text>
+                </View>
+    }
 }
 class HomeScreen extends Component {
     constructor(props) {
         super(props);
         this.state = {weather: [], sevenDays: [], zipCode: '', text: '', location: null, errorMessage: null, longitude: null, latitude: null, displayLocation: "...", loadingImage: 250};
     }
-
-
 
     componentWillMount() {
         if (Platform.OS === 'android' && !Constants.isDevice) {
@@ -45,37 +45,44 @@ class HomeScreen extends Component {
     }
 
     _getLocationAsync = async () => {
-    let { status } = await Permissions.askAsync(Permissions.LOCATION);
-    if (status !== 'granted') {
-        this.setState({
-            errorMessage: 'Permission to access location was denied',
-        });
-    }
-
-    let location = await Location.getCurrentPositionAsync({});
-    this.setState({ location });
-    this.lookupWeather();
+        let { status } = await Permissions.askAsync(Permissions.LOCATION);
+        if (status !== 'granted') {
+            this.setState({
+                errorMessage: 'Permission to access location was denied',
+            });
+        }
+        let location = await Location.getCurrentPositionAsync({});
+        this.setState({ location });
+        this.lookupWeather();
     };
 
 
     lookupWeather = () => {
-    const that = this;
+        const that = this;
+
+        //Look up weather by zip code entered
         if(this.state.text.length > 0){
-        fetch('http://api.openweathermap.org/data/2.5/forecast/daily?zip=' + this.state.text + '&APPID=916847b6b7d2d1d9a152a66bf14d2095&units=imperial')
-            .then(res => res.json())
-            .then(weather => this.setState({weather: weather}))
-            .catch(err => console.log('There was an error fetching the weather:' + err));
-        myWeather(this.state.weather['list'])
-            this.state.displayLocation = this.state.text;
+            fetch('http://api.openweathermap.org/data/2.5/forecast/daily?zip=' + this.state.text + '&APPID=916847b6b7d2d1d9a152a66bf14d2095&units=imperial')
+                .then(res => res.json())
+                .then(weather => this.setState({weather: weather}))
+                .catch(err => console.log('There was an error fetching the weather:' + err));
+                RenderSevenDayWeather(this.state.weather['list'], this.state.text)
+                this.state.displayLocation = this.state.text;
         }
+
+        // Look up weather by users geo location
         else if(this.state.location !== null){
             fetch('http://api.openweathermap.org/data/2.5/forecast/daily?'+ this.state.latitude + this.state.longitude + '&APPID=916847b6b7d2d1d9a152a66bf14d2095&units=imperial')
                 .then((res) => res.json())
-                .then(function(data) { that.setState({weather: data}); that.setState({displayLocation: data.city.name});})
+                .then(function(data) {
+                    that.setState({weather: data});
+                    that.setState({displayLocation: data.city.name});
+                })
                 .catch(err => console.log('There was an error fetching the weather:' + err));
-            myWeather(this.state.weather['list']);
+                RenderSevenDayWeather(this.state.weather['list'], "geolocationused");
         }
     }
+
     render() {
         let loadingWeatherMessage = "Loading weather for your current location"
         if (this.state.errorMessage) {
@@ -86,7 +93,6 @@ class HomeScreen extends Component {
             loadingWeatherMessage = "";
             this.state.loadingImage = 0;
         }
-
 
         return (
             <View style={styles.containerSuper}>
@@ -99,6 +105,7 @@ class HomeScreen extends Component {
                         placeholder="Zip Code"
                         underlineColorAndroid='transparent'
                         keyboardType = 'numeric'
+                        maxLength = {5}
                         onChangeText={(text) => this.setState({text})}
                     />
                     <View style={styles.button}>
@@ -110,10 +117,10 @@ class HomeScreen extends Component {
                         />
                     </View>
                 </View>
-                                <View style={{alignItems: 'center', justifyContent: 'center' }}>
-                                <Image style={{height: this.state.loadingImage, width: this.state.loadingImage}} source={require('../images/loading.gif')} />
-                                </View>
-                    {myWeather(this.state.weather['list'])}
+                <View style={{alignItems: 'center', justifyContent: 'center' }}>
+                    <Image style={{height: this.state.loadingImage, width: this.state.loadingImage}} source={require('../images/loading.gif')} />
+                </View>
+                <View style={styles.invalidZipCodeContainer}>{RenderSevenDayWeather(this.state.weather['list'], this.state.text)}</View>
             </View>
         );
     }
@@ -163,6 +170,20 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
         flexDirection: 'row',
+    },
+    invalidZipCodeContainer: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'row',
+        paddingTop: 15
+    },
+    invalidZipCode: {
+        fontSize: 15,
+        fontWeight: "600",
+        color: "red"
+    },
+    invalZipCodeMessage: {
+        fontSize: 18,
     }
 });
 export default HomeScreen;
